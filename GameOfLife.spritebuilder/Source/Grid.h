d91cda7bfbcbb062f57203326ab3a1410f667bd9
Abhishek Mukhopadhyay <mukho@Sreejitas-MacBook-Pro.local>
2015-02-09 04:34:07 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,14 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Abhishek Mukhopadhyay on 2/9/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+@end
